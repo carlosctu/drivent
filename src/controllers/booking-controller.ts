01ec903be0d08c7ac085ae9298c5537ca998ed49
carlos_casanova <carlosctu94@gmail.com>
2022-12-04 20:21:03 -0300

    feat/adding route names

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -13,6 +13,36 @@ export async function getUserBooking(req: AuthenticatedRequest, res: Response) {
     if (error.name === "NotFoundError") {
       return res.sendStatus(httpStatus.NOT_FOUND);
     }
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    return res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
+export async function createNewBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const roomId = req.body;
+
+  try {
+    const booking = await bookingService.insertBooking(userId, roomId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    return res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
+
+export async function updateUserBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const id = req.params;
+  const roomId = req.body;
+
+  try {
+    const booking = await bookingService.updateBooking(userId, Number(id), roomId );
+    return res.status(httpStatus.OK).send(booking);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    return res.sendStatus(httpStatus.FORBIDDEN);
   }
 }
--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { getUserBooking } from "@/controllers";
+import { createNewBooking, getUserBooking, updateUserBooking } from "@/controllers";
 
 const bookingRouter = Router();
 
-bookingRouter.all("/*", authenticateToken).get("/", getUserBooking);
+bookingRouter
+  .all("/*", authenticateToken)
+  .get("/", getUserBooking)
+  .post("/", createNewBooking)
+  .put("/:bookingId", updateUserBooking);
 
 export { bookingRouter };
